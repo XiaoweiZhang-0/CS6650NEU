4580aa3ea8fda9cc1e669f5a2b42d2668ce8eec6
Xiaowei Zhang <blackcatecho1998@gmail.com>
2023-10-18 19:50:21 -0700

    clean build and make changes to servlet

--- Assignment/javaservlets/src/main/java/com/example/MusicAlbumServlet.java
+++ Assignment/javaservlets/src/main/java/com/example/MusicAlbumServlet.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,70 +11,79 @@ import java.util.logging.Logger;
 
 @WebServlet(name = "MusicAlbumServlet", urlPatterns = {"/albums/*"})
 public class MusicAlbumServlet extends HttpServlet{
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        res.setContentType("text/plain");
-        String urlPath = req.getPathInfo();
+    //Get 
+    // @Override
+    // protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    //     res.setContentType("text/plain");
+    //     String urlPath = req.getPathInfo();
     
-        // check we have a URL!
-        if (urlPath == null || urlPath.isEmpty()) {
-            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            res.getWriter().write("missing paramterers");
-            return;
-        }
+    //     // check we have a URL!
+    //     if (urlPath == null || urlPath.isEmpty()) {
+    //         res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+    //         res.getWriter().write("missing paramterers");
+    //         return;
+    //     }
     
-        String[] urlParts = urlPath.split("/");
-        // and now validate url path and return the response status code
-        // (and maybe also some value if input is valid)
+    //     String[] urlParts = urlPath.split("/");
+    //     // and now validate url path and return the response status code
+    //     // (and maybe also some value if input is valid)
     
-        if (!isGetUrlValid(urlParts)) {
-            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            res.getWriter().write("not valid");
-            // res.getWriter().write(urlParts[0]);
-        } else {
-            res.setStatus(HttpServletResponse.SC_OK);
-            // do any sophisticated processing with urlParts which contains all the url params
-            // TODO: process url params in `urlParts`
-            res.getWriter().write("It works!");
-        }
-        return;
-    }
+    //     if (!isGetUrlValid(urlParts)) {
+    //         res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+    //         res.getWriter().write("not valid");
+    //         // res.getWriter().write(urlParts[0]);
+    //     } else {
+    //         res.setStatus(HttpServletResponse.SC_OK);
+    //         // do any sophisticated processing with urlParts which contains all the url params
+    //         // TODO: process url params in `urlParts`
+    //         res.getWriter().write("It works!");
+    //     }
+    //     return;
+    // }
    
-    private boolean isGetUrlValid(String[] urlParts) {
-        // TODO: validate the request url path according to the API spec
-        // urlParts = [, 1, seasons, 2019, day, 1, skier, 123]
-        if(urlParts.length != 1) {
-            return false;
-        }
-        return true;
-    }
+    // private boolean isGetUrlValid(String[] urlParts) {
+    //     // TODO: validate the request url path according to the API spec
+    //     // urlParts = [, 1, seasons, 2019, day, 1, skier, 123]
+    //     if(urlParts.length != 1) {
+    //         return false;
+    //     }
+    //     return true;
+    // }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        res.setContentType("text/plain");
-        String urlPath = req.getPathInfo();
+        res.setContentType("application/json");
+        // String urlPath = req.getPathInfo();
     
-        // check we have a URL!
-        if (urlPath == null || urlPath.isEmpty()) {
-            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            res.getWriter().write("missing paramterers");
-            return;
+        // // check we have a URL!
+        // if (urlPath == null || urlPath.isEmpty()) {
+        //     res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        //     res.getWriter().write("missing paramterers");
+        //     return;
+        // }
+        StringBuilder requestBody = new StringBuilder();
+        String line;
+        try (BufferedReader reader = req.getReader()) {
+            while ((line = reader.readLine()) != null) {
+                requestBody.append(line);
+            }
         }
-    
-        String[] urlParts = urlPath.split("/");
+        System.out.println(requestBody.toString());
+        
+        // String[] urlParts = urlPath.split("/");
         // and now validate url path and return the response status code
         // (and maybe also some value if input is valid)
     
-        if (!isPostUrlValid(urlParts)) {
-            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        } else {
-            res.setStatus(HttpServletResponse.SC_OK);
-            // do any sophisticated processing with urlParts which contains all the url params
-        }
+        // if (!isPostUrlValid(urlParts)) {
+        //     res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        // } else {
+        //     res.setStatus(HttpServletResponse.SC_OK);
+        //     // do any sophisticated processing with urlParts which contains all the url params
+        // }
     }
-    private boolean isPostUrlValid(String[] urlParts) {
-        return true;
-    }
+    // private boolean isPostUrlValid(String[] urlParts) {
+    //     return true;
+    // }
 
 
 
